1b4b4b41a2d1a632b9ec32c3bfc7e94b2db3a732
Matthew Troost <matthewtroost@Matthews-MBP.Dlink>
2019-07-11 23:12:45 +0200

    Added grid and started writing filter function

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../Assets/dinoparks-logo.png';
+import '../Styles/App.css';
+import Grid from './Grid';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="logo">
+          <img alt="DinoPark" src={logo} />
+        </div>
+        <Switch>
+          <Route exact path="/" component={Grid} />
+          <Route render={() => <h1>Whoops.. Page not found :(</h1>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,10 +1,116 @@
 import React, { Component } from "react";
 import '../Styles/Custom.css';
+import { times, find, forEach, remove, findIndex } from 'lodash';
+import axios from 'axios';
 
 class Grid extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            logs: [],
+            dinosaurs: []
+        }
+    }
+
+    fetchData() {
+        axios.get('https://dinoparks.net/nudls/feed')
+            .then((response) => {
+                this.setState({
+                    logs: response.data
+                });
+
+                this.filterData();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    filterData() {
+        const dinosaurs = [];
+
+        forEach(this.state.logs, function (log) {
+
+            var index = findIndex(dinosaurs, { id: log.dinosaur_id });
+            var dino = find(dinosaurs, ['id', log.dinosaur_id]);
+
+            switch (log.kind) {
+                case 'dino_added':
+                    index = findIndex(dinosaurs, { id: log.id });
+                    if (index > -1) {
+                        dinosaurs[index].digestion_period = log.digestion_period_in_hours;
+                    } else {
+                        dinosaurs.push(
+                            {
+                                id: log.id,
+                                digestion_period: log.digestion_period_in_hours
+                            });
+                    }
+                    break;
+                case 'dino_removed':
+                    if (dino) {
+                        remove(dinosaurs, function (existingDino) {
+                            return existingDino === dino;
+                        });
+                    }
+                    else {
+                        dinosaurs.push(
+                            {
+                                id: log.dinosaur_id,
+                                removed: true
+                            });
+                    }
+                    break;
+                case 'dino_location_updated':
+                    if (dino) {
+                        if (dino.location) {
+
+                        }
+                        else {
+                            dinosaurs[index].location = log.location;
+                            dinosaurs[index].time_of_locating = log.time;
+                        }
+                    }
+                    else {
+                        dinosaurs.push(
+                            {
+                                id: log.dinosaur_id,
+                                location: log.location,
+                                time_of_locating: log.time
+                            });
+                    }
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        this.setState({
+            dinosaurs: dinosaurs
+        });
+    }
+
     render() {
+        const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
         return (
+            <div>
+                <button onClick={() => this.fetchData()}>Fetch Data</button>
+                {times(16, (i) =>
+                    <div className="row">
+                        <span>{i + 1}</span>
+                        {times(26, (x) =>
+                            <div className="block">
+                                {alphabet[x] + (i + 1)}
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+
         );
     }
 
